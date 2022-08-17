35c601c2a6980c3882063febfcfc4b227afcb79f
Bhavyakaushal <bhavya.k@smarter.codes>
2022-08-17 21:13:54 +0530

    Swagger api added

--- nest-server/dist/main.js
+++ nest-server/dist/main.js
@@ -2,9 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle("CV Builder")
+        .setDescription("This is cv builder")
+        .setVersion("1.0")
+        .addTag("users")
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup("api", app, document);
     await app.listen(8080);
 }
 bootstrap();
--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
+
+	const config = new DocumentBuilder()
+		.setTitle("CV Builder")
+		.setDescription("This is cv builder")
+		.setVersion("1.0")
+		.addTag("users")
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("api", app, document);
 	await app.listen(8080);
 }
 bootstrap();
